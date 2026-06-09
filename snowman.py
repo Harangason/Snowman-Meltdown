b555674d404747c9b348b290256152354d126357
Harangason <martin-j.drexler@web.de>
2026-06-09 13:38:58 +0200

    create try-except block for JSON file handling

--- snowman.py
+++ snowman.py
@@ -19,12 +19,22 @@ def play_game():
 
     # TODO: Build your game loop here.
     # JSON-Datei öffnen und laden
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-
-    # Verbindet den Ordner sauber mit dem Dateinamen
-    json_path = os.path.join(script_dir, "snowman.json")
-    with open(json_path, "r", encoding="utf-8") as file:
-        data = json.load(file)
+    try:
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        # Verbindet den Ordner sauber mit dem Dateinamen
+        json_path = os.path.join(script_dir, "snowman.json")
+        with open(json_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+        if "snowman" not in data:
+            print("No snowman found in the JSON file.")
+            return
+        if not isinstance(data["snowman"], list):
+            print("snowman is not a list.")
+        else:
+            print("snowman is a list.")
+            break
+    except FileNotFoundError:
+        print(("snowman.json not found."))
 
     # Den Schneemann aus der Liste Zeile für Zeile ausgeben
     for line in data["snowman"]:
