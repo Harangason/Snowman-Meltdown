40b9c6f455e2938c1369883df54293a060fb166b
Harangason <martin-j.drexler@web.de>
2026-06-09 13:31:58 +0200

    Create snowman.json import robust and import os for JSON file path

---
+++ snowman.py
@@ -0,0 +1,44 @@
+import random
+import json
+import os
+# List of secret words
+
+
+WORDS = ["python", "git", "github", "snowman", "meltdown"]
+
+
+def get_random_word():
+    """Selects a random word from the list."""
+    return WORDS[random.randint(0, len(WORDS) - 1)]
+
+
+def play_game():
+    secret_word = get_random_word()
+    print("Welcome to Snowman Meltdown!")
+    print("Secret word selected: " + secret_word)  # for testing, later remove this line
+
+    # TODO: Build your game loop here.
+    # JSON-Datei öffnen und laden
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Verbindet den Ordner sauber mit dem Dateinamen
+    json_path = os.path.join(script_dir, "snowman.json")
+    with open(json_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    # Den Schneemann aus der Liste Zeile für Zeile ausgeben
+    for line in data["snowman"]:
+        print(line)
+
+
+
+    # For now, simply prompt the user once:
+    guess = input("Guess a letter: ").lower()
+    print("You guessed:", guess)
+
+
+if __name__ == "__main__":
+    play_game()
+
+
+
