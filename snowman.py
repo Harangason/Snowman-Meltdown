c0c8c7c9199f528ad2531e5e14f2dae01ca8a297
Harangason <martin-j.drexler@web.de>
2026-06-10 09:41:25 +0200

    issue solution... after extract this "function" in an def function, wtihout own loop

--- snowman.py
+++ snowman.py
@@ -41,10 +41,12 @@ def get_guess():
     # Eingabe vom Spieler holen
     guess = input("\nGuess a letter: ").strip().lower()
 
-    # Validierung: Eingabe darf nicht leer sein und sollte nur 1 Buchstabe sein
-    if not guess or len(guess) != 1 or not guess.isalpha():
-        print("Ungültige Eingabe. Bitte gib genau einen Buchstaben ein.")
-        continue
+        # Validierung: Eingabe darf nicht leer sein und sollte nur 1 Buchstabe sein
+        if not guess or len(guess) != 1 or not guess.isalpha():
+            print("Ungültige Eingabe. Bitte gib genau einen Buchstaben ein.")
+            continue
+        else:
+            break
     return guess
 
 def play_game():
